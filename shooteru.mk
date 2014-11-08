c41b7fc25978aa32267c58a0ad50661f77ffdafb
Dogukan Ergun <dogukan.ergun@gmail.com>
2014-11-08 17:00:43 +0200

    Add aosp telephony

--- shooteru.mk
+++ shooteru.mk
@@ -14,8 +14,17 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/htc/shooteru/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+PRODUCT_NAME := shooteru
+PRODUCT_DEVICE := shooteru
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on shooteru
+PRODUCT_MANUFACTURER := HTC
 
 # common msm8660 configs
 $(call inherit-product, device/htc/msm8660-common/msm8660.mk)
