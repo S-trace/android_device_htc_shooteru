f3d1ff8222288741348ab7b022e73b4f7d8f85c7
Ivan Grinko <iivanich@gmail.com>
2014-11-18 12:21:40 +0200

    shooteru: Clean up media codecs
    * Use common Google codec definitions
    
    Conflicts:
    	configs/media_codecs.xml
    	device_pyramid.mk

--- shooteru.mk
+++ shooteru.mk
@@ -164,6 +164,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.locationfeatures=1 \
     dalvik.vm.dexopt-flags=m=y
 
+# Media configuration
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
 # ADB
 ADDITIONAL_DEFAULT_PROPERTIES+=  ro.adb.secure=0
 
