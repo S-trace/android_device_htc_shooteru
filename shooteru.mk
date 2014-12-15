0b51d7cf016c58b83e106be8b8acf866b8bd5aeb
Dogukan Ergun <dogukan.ergun@gmail.com>
2014-12-15 16:43:14 +0200

    add configs

--- shooteru.mk
+++ shooteru.mk
@@ -172,7 +172,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/audio_policy.xml:system/etc/audio_policy.xml \
+    $(LOCAL_PATH)/configs/audio_effects.xml:system/etc/audio_effects.xml 
 
 # ADB
 ADDITIONAL_DEFAULT_PROPERTIES+=  ro.adb.secure=0
