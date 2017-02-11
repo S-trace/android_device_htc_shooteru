6c0639b11daddb8670c1443c386cb6686eb0b0a1
S-trace <S-trace@list.ru>
2017-02-12 00:26:34 +0300

    [CONFIGS] wlan: Add wpa_supplicant.conf
    Fixes WLAN failure:
    E/WifiMonitor(  567): killSupplicant p2pfalse init.svc.wpa_supplicant=unknown init.svc.p2p_supplicant=unknown
    E/WifiHW  (  567): Cannot open "/system/etc/wifi/wpa_supplicant.conf": No such file or directory
    E/WifiHW  (  567): Wi-Fi will not be enabled
    E/WifiStateMachine(  567): Failed to start supplicant!
    
    Change-Id: I03de4c423a3cbf7621757adb18e60bd3f03d6ec3

--- shooteru.mk
+++ shooteru.mk
@@ -128,6 +128,10 @@ PRODUCT_PACKAGES += \
 ## The gps config appropriate for this device
 PRODUCT_COPY_FILES += device/common/gps/gps.conf_EU:system/etc/gps.conf
 
+# WLAN
+PRODUCT_PACKAGES += \
+    wpa_supplicant.conf
+
 # Kernel init.d script
 #PRODUCT_COPY_FILES += \
 #    device/htc/shooteru/configs/99kernel:system/etc/init.d/99kernel \
